25b2a22d82d392eafc6417616c5d6388793d12ea
minhquanghaiquan <minhquanghaiquan@gmail.com>
2020-06-08 11:26:50 +0200

    demo-functionalstateless

--- src/components/TrafficLight.js
+++ src/components/TrafficLight.js
@@ -6,23 +6,18 @@ const RED = 0 ;
 const ORANGE = 1 ;
 const GREEN = 2 ;
 
-class TrafficLight extends Component {
-    
-
-    render() {
-        const { currentColor} = this.props;
-        return <div className="TrafficLight">
-            <div className={classNames('bulb', 'red', {
-                active: currentColor === RED
-            })} />
-            <div className={classNames('bulb', 'orange', {
-                active: currentColor === ORANGE
-            })} />
-            <div className={classNames('bulb', 'green', {
-                active: currentColor === GREEN
-            })} />
+function TrafficLight(props){
+    const { currentColor} = props;
+    return <div className="TrafficLight">
+        <div className={classNames('bulb', 'red', {
+            active: currentColor === RED
+        })} />
+        <div className={classNames('bulb', 'orange', {
+            active: currentColor === ORANGE
+        })} />
+        <div className={classNames('bulb', 'green', {
+            active: currentColor === GREEN
+        })} />
         </div>;
-    }
 }
-
 export default TrafficLight;
